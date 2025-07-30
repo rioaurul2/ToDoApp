d441eee1d35ac0a84799cbec96d496b6f12ef8a9
rioaurul2 <pmarin08@yahoo.com>
2025-07-30 18:56:35 +0300

    single 3

---
+++ DesignPatternsPractice/Creational/Singleton/Exercitii/3/DbSingleton.cs
@@ -0,0 +1,22 @@
+﻿namespace DesignPatternsPractice.Creational.Singleton.Exercitii._3;
+
+public class DbSingleton
+{
+    private static DbSingleton _instance;
+    private static readonly object _lock = new object();
+
+    private DbSingleton() { }
+
+    public static DbSingleton GetInstance()
+    {
+        lock (_lock)
+        {
+            if(_instance == null)
+            {
+                _instance = new DbSingleton();
+            }
+
+            return _instance;
+        }
+    }
+}
